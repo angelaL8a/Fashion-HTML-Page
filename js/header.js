6c46332848903ed4efd5526d12046e67006318f2
Angela <aplozano2004@gmail.com>
2023-07-11 19:00:50 -0500

    nada

--- js/header.js
+++ js/header.js
@@ -1,12 +1,82 @@
+// Header
 var headerElement = document.getElementById("header");
 headerElement.innerHTML = `
-  <div class="header_logo">Logo</div>
-    <div class="header_links">
-      <a href="#">Home</a>
-      <a href="#">Fashion</a>
-      <a href="#">Beauty</a>
-      <a href="#">Culture</a>
-      <a href="#">About</a>
-    </div>
-  <div class="header_search">Search</div>
+  <a href="/" class="header_logo">Logo</a>
+
+  <div class="header_links" id="header_links"></div>
+
+  <div class="header_search">Search</div> 
 `;
+
+// Header links
+const categories = [
+  {
+    name: "News",
+    sub_cat: [
+      { name: "Awards and Events" },
+      { name: "Music Culture" },
+      { name: "Entertainment" },
+    ],
+  },
+  {
+    name: "Fashion",
+    sub_cat: [
+      { name: "Models" },
+      { name: "Clothing" },
+      { name: "Designers & Trends" },
+    ],
+  },
+  {
+    name: "Beauty",
+    sub_cat: [
+      { name: "Celebrity" },
+      { name: "Makeup & Skincare" },
+      { name: "Fragance" },
+    ],
+  },
+  {
+    name: "Culture",
+    sub_cat: [{ name: "Music" }, { name: "Arts" }, { name: "Sports" }],
+  },
+  {
+    name: "Lifestyle",
+    sub_cat: [
+      { name: "Travel" },
+      { name: "Home & Decorating" },
+      { name: "Holidays" },
+    ],
+  },
+];
+
+const categoriesContainer = document.getElementById("header_links");
+
+categories.forEach((item, index) => {
+  const itemElement = document.createElement("div");
+  itemElement.classList.add("header_link");
+  itemElement.classList.add("group");
+  itemElement.classList.add(`link_${index}`);
+
+  itemElement.innerHTML = `
+    <a href="#">${item.name}</a>
+
+    <div id="link_card_${index}" class="link_card group-hover:flex hidden">
+      <div class="arrow_container">
+        <div class="arrow"></div>
+      </div>
+    </div>`;
+
+  categoriesContainer.appendChild(itemElement);
+
+  const subContainer = document.getElementById(`link_card_${index}`);
+
+  item.sub_cat.forEach((sub, sub_index) => {
+    const sub_item = document.createElement("a");
+    sub_item.href = "#";
+    sub_item.classList.add("sub_link");
+
+    sub_item.innerHTML = `
+      <span>${sub.name}</span>
+    `;
+    subContainer.appendChild(sub_item);
+  });
+});
--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// Grid section (index.html)
 const elements = [
   {
     title: "Test1",
@@ -32,6 +33,7 @@ elements.forEach((element) => {
   const articleElement = document.createElement("div");
   articleElement.classList.add("article");
   articleElement.classList.add(`${element.index}`);
+
   articleElement.innerHTML = `
           <div class="article_img">
             <img src=${element.src} alt="card image 1" />
@@ -42,5 +44,94 @@ elements.forEach((element) => {
             </h4>
           </div>
   `;
+
   articlesContainer.appendChild(articleElement);
+});
+
+// Must read section (index.html)
+const mustReadElements = [
+  {
+    category: "Fashion",
+    title: "String",
+    src: "https://assets.vogue.com/photos/64ac10920def6fd42956fb15/1:1/w_120,c_limit/Dua%20Lipa_100723_GettyImages-1529735402.jpg%20120w,%20https://assets.vogue.com/photos/64ac10920def6fd42956fb15/1:1/w_240,c_limit/Dua%20Lipa_100723_GettyImages-1529735402.jpg%20240w,%20https://assets.vogue.com/photos/64ac10920def6fd42956fb15/1:1/w_320,c_limit/Dua%20Lipa_100723_GettyImages-1529735402.jpg%20320w",
+  },
+  {
+    category: "Culture",
+    title: "String",
+    src: "https://assets.vogue.com/photos/64ac10920def6fd42956fb15/1:1/w_120,c_limit/Dua%20Lipa_100723_GettyImages-1529735402.jpg%20120w,%20https://assets.vogue.com/photos/64ac10920def6fd42956fb15/1:1/w_240,c_limit/Dua%20Lipa_100723_GettyImages-1529735402.jpg%20240w,%20https://assets.vogue.com/photos/64ac10920def6fd42956fb15/1:1/w_320,c_limit/Dua%20Lipa_100723_GettyImages-1529735402.jpg%20320w",
+  },
+  {
+    category: "Beauty",
+    title: "String",
+    src: "https://assets.vogue.com/photos/64ac10920def6fd42956fb15/1:1/w_120,c_limit/Dua%20Lipa_100723_GettyImages-1529735402.jpg%20120w,%20https://assets.vogue.com/photos/64ac10920def6fd42956fb15/1:1/w_240,c_limit/Dua%20Lipa_100723_GettyImages-1529735402.jpg%20240w,%20https://assets.vogue.com/photos/64ac10920def6fd42956fb15/1:1/w_320,c_limit/Dua%20Lipa_100723_GettyImages-1529735402.jpg%20320w",
+  },
+];
+
+const mustReadContainer = document.getElementById("must_read_articles");
+mustReadElements.forEach((i, index) => {
+  const item = document.createElement("div");
+  item.classList.add("must_read_art");
+
+  item.innerHTML = `
+    <div class="must_read_art_img">
+      <img
+        style="width: 100%; height: 100%; object-fit: cover"
+        src="${i.src}"
+      />
+    </div>
+
+    <div>
+      <a href="#">${i.category}</a>
+      <p>${i.title}</p>
+    </div>
+  `;
+  mustReadContainer.appendChild(item);
+});
+
+// Celebrity Style section (index.html)
+const celebrityItems = [
+  {
+    category: "Fashion",
+    title: "La billie",
+    src: "https://assets.vogue.com/photos/64ac4b21406cf236781d59d2/4:3/w_1920,c_limit/1530545876",
+  },
+  {
+    category: "Fashion",
+    title: "La billie",
+    src: "https://assets.vogue.com/photos/64ac4b21406cf236781d59d2/4:3/w_1920,c_limit/1530545876",
+  },
+  {
+    category: "Fashion",
+    title: "La billie",
+    src: "https://assets.vogue.com/photos/64ac4b21406cf236781d59d2/4:3/w_1920,c_limit/1530545876",
+  },
+  {
+    category: "Fashion",
+    title: "La billie",
+    src: "https://assets.vogue.com/photos/64ac4b21406cf236781d59d2/4:3/w_1920,c_limit/1530545876",
+  },
+];
+
+const celebrityContainer = document.getElementById(
+  "celebrity_section_container"
+);
+
+celebrityItems.forEach((c, index) => {
+  const item = document.createElement("div");
+  item.classList.add("celebrity_section_art");
+
+  item.innerHTML = `
+    <div>
+      <img
+        src="${c.src}"
+        alt="labillie"
+      />
+    </div>
+    <div class="celebrity_section_art_content">
+      <a href="#">${c.category}</a>
+      <p>${c.title}</p>
+    </div>
+  `;
+
+  celebrityContainer.appendChild(item);
 });
